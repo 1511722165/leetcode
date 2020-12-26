c4ff85fd68713c111669c3d397061083c3c124b0
huangjian <1511722165@qq.com>
2020-12-26 10:18:18 +0800

    748

---
+++ src/leetcode/leetcode0001_1000/leetcode701_800/leetcode0741_0750/LeetCode0748.java
@@ -0,0 +1,17 @@
+package leetcode.leetcode0001_1000.leetcode701_800.leetcode0741_0750;
+
+public class LeetCode0748 {
+
+	 public int minCostClimbingStairs(int[] cost) {
+		 int result=cost[0];
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 return result; 	
+	    }
+	
+}
