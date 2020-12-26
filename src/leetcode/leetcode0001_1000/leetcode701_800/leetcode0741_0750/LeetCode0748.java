38f8170afc08ea8fc342db27ba0a38da4bd00c77
huangjian <1511722165@qq.com>
2020-12-26 11:27:06 +0800

    todo

--- src/leetcode/leetcode0001_1000/leetcode701_800/leetcode0741_0750/LeetCode0748.java
+++ src/leetcode/leetcode0001_1000/leetcode701_800/leetcode0741_0750/LeetCode0748.java
@@ -2,16 +2,40 @@ package leetcode.leetcode0001_1000.leetcode701_800.leetcode0741_0750;
 
 public class LeetCode0748 {
 
-	 public int minCostClimbingStairs(int[] cost) {
-		 int result=cost[0];
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 return result; 	
-	    }
-	
+	public int minCostClimbingStairs(int[] cost) {
+//		if (cost.length <= 1) {
+//			return 0;
+//		}
+//		int result = 0;
+//		int i = 0;
+//		int k=1;
+//		for (; i < cost.length - 3; i++) {
+//			if (cost[i] + cost[i + 2] > cost[i + 1]) {
+//				result = result + cost[i + 1];
+//				i++;
+//			} else {
+//				result = result + cost[i];
+//			}
+//		}
+//		if (cost.length - i == 2) {
+//			int res = cost[i + 1] > cost[i] ? cost[i] : cost[i + 1];
+//			result = result + res;
+//			return result;
+//		} else {
+//			int res = cost[i] + cost[i + 2] > cost[i + 1] ? cost[i + 1] : cost[i] + cost[i + 2];
+//			result = result + res;
+//			return result;
+//
+//		}
+		return 1;
+
+	}
+
+	public static void main(String[] args) {
+		LeetCode0748 demo = new LeetCode0748();
+		int[] cost = { 0, 0, 0, 0 };
+		System.out.println(demo.minCostClimbingStairs(cost));
+
+	}
+
 }
