a0e2416124668615ed5fba6944ce91e8d17ddf6e
huangjian <1511722165@qq.com>
2020-12-28 22:15:16 +0800

    0035

--- src/leetcode/leetcode0001_1000/leetcode001_100/leetcode0031_0040/LeetCode0035.java
+++ src/leetcode/leetcode0001_1000/leetcode001_100/leetcode0031_0040/LeetCode0035.java
@@ -4,9 +4,14 @@ public class LeetCode0035 {
 
 	public int searchInsert(int[] nums, int target) {
 		for (int i = 0; i < nums.length; i++) {
-			
-		}
+			if (nums[i] == target) {
+				return i;
+			} else if (nums[i] < target) {
 
-		return 0;
+			} else {
+				return i;
+			}
+		}
+		return nums.length;
 	}
 }
