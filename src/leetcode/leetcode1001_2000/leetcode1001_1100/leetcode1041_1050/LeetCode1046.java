012a564274bd87c81185a48c488f64af7b5aefa1
huangjian <1511722165@qq.com>
2020-12-30 00:51:02 +0800

    1

---
+++ src/leetcode/leetcode1001_2000/leetcode1001_1100/leetcode1041_1050/LeetCode1046.java
@@ -0,0 +1,15 @@
+package leetcode.leetcode1001_2000.leetcode1001_1100.leetcode1041_1050;
+
+public class LeetCode1046 {
+	
+	 public int lastStoneWeight(int[] stones) {
+		 
+		 
+		 
+		 
+		 
+		 return 0;
+	        
+	    }
+
+}
