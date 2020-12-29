e96bf4052cc931943b6c2f2e410427e1a2eb2060
huangjian <1511722165@qq.com>
2020-12-29 11:29:57 +0800

    todo

---
+++ src/leetcode/leetcode0001_1000/leetcode001_100/leetcode0061_0070/LeetCode0067.java
@@ -0,0 +1,14 @@
+package leetcode.leetcode0001_1000.leetcode001_100.leetcode0061_0070;
+
+public class LeetCode0067 {
+	
+	public String addBinary(String a, String b) {
+		if(a.length()>b.length()){
+			
+		}else{
+			
+			String news=String.format(a, b);
+		}
+    }
+
+}
