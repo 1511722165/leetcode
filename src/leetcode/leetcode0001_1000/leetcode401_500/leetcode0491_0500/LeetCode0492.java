44f656f4ed8e9da51bc62e8a07e5893736e067c9
huangjian <1511722165@qq.com>
2020-12-30 13:46:40 +0800

    update

--- src/leetcode/leetcode0001_1000/leetcode401_500/leetcode0491_0500/LeetCode0492.java
+++ src/leetcode/leetcode0001_1000/leetcode401_500/leetcode0491_0500/LeetCode0492.java
@@ -3,22 +3,12 @@ package leetcode.leetcode0001_1000.leetcode401_500.leetcode0491_0500;
 public class LeetCode0492 {
 
 	public int[] constructRectangle(int area) {
-		int[] nums = new int[2];
-		int a = (int) Math.sqrt(area);
-		if (Math.pow(a, 2) == area) {
-			nums[1] = a;
-			nums[0] = a;
-			return nums;
-		}
-		a = a + 1;
-		for (; a <= area; a++) {
+		for (int a = (int) Math.sqrt(area); a >= 0; a--) {
 			if (area % a == 0) {
-				nums[1] = area / a;
-				nums[0] = a;
-				return nums;
+				return new int[] { area / a, a };
 			}
 		}
-		return nums;
+		return null;
 	}
 
 	public static void main(String[] args) {
