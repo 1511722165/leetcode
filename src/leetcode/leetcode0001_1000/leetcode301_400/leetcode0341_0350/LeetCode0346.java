56cf688246fa4806ed225137908789ba7d87a702
huangjian <1511722165@qq.com>
2020-12-30 11:29:18 +0800

    46

---
+++ src/leetcode/leetcode0001_1000/leetcode301_400/leetcode0341_0350/LeetCode0346.java
@@ -0,0 +1,20 @@
+package leetcode.leetcode0001_1000.leetcode301_400.leetcode0341_0350;
+
+public class LeetCode0346 {
+	public boolean isPowerOfFour(int n) {
+		
+		if(n==1){
+			return true;
+		}
+		
+		if(n>>4==0){
+			
+		}else{
+			
+		}
+		
+		return false;
+		
+    }
+
+}
