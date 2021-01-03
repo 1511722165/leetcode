1465a549bc1c3e5bce97be0a55747cffb74323bf
huangjian <1511722165@qq.com>
2021-01-03 10:43:48 +0800

    121

--- src/leetcode/leetcode0001_1000/leetcode101_200/leetcode0121_0130/LeetCode0121.java
+++ src/leetcode/leetcode0001_1000/leetcode101_200/leetcode0121_0130/LeetCode0121.java
@@ -2,4 +2,26 @@ package leetcode.leetcode0001_1000.leetcode101_200.leetcode0121_0130;
 
 public class LeetCode0121 {
 
+	public int maxProfit(int[] prices) {
+		if (prices.length <= 1) {
+			return 0;
+		}
+		int max = 0;
+		int min = prices[0];
+		for (int i = 1; i < prices.length; i++) {
+			// min = Math.min(min, prices[i - 1]);
+			min = min > prices[i - 1] ? prices[i - 1] : min;
+			// max = Math.max(prices[i] - min, max);
+			max = max > prices[i] - min ? max : prices[i] - min;
+
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		LeetCode0121 demo = new LeetCode0121();
+		int[] prices = { 7, 1, 5, 3, 6, 4 };
+		demo.maxProfit(prices);
+
+	}
 }
