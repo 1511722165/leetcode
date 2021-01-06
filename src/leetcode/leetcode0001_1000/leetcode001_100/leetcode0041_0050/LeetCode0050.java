7350504f7041a3b6189ae1f3457f335a3ee73d5d
huangjian <1511722165@qq.com>
2021-01-06 11:29:39 +0800

    1

---
+++ src/leetcode/leetcode0001_1000/leetcode001_100/leetcode0041_0050/LeetCode0050.java
@@ -0,0 +1,7 @@
+package leetcode.leetcode0001_1000.leetcode001_100.leetcode0041_0050;
+
+public class LeetCode0050 {
+	public double myPow(double x, int n) {
+		return Math.pow(x, n);
+	}
+}
---
+++ src/leetcode/leetcode0001_1000/leetcode101_200/leetcode0121_0130/LeetCode0125.java
@@ -0,0 +1,7 @@
+package leetcode.leetcode0001_1000.leetcode101_200.leetcode0121_0130;
+
+public class LeetCode0125 {
+	public boolean isPalindrome(String s) {
+
+    }
+}
