a05a689486a0a0a5f4ff266520b86573e2ad22df
huangjian <1511722165@qq.com>
2021-01-08 00:37:03 +0800

    3432

---
+++ src/leetcode/leetcode0001_1000/leetcode201_300/leetcode0201_0210/LeetCode0201.java
@@ -0,0 +1,39 @@
+package leetcode.leetcode0001_1000.leetcode201_300.leetcode0201_0210;
+
+public class LeetCode0201 {
+
+	public int rangeBitwiseAnd(int m, int n) {
+		int count = n - m;
+		if (count == 0) {
+			return n;
+		} else {
+			String string = Integer.toBinaryString(n);
+			StringBuilder sb = new StringBuilder();
+			if (string.length() <= count) {
+				return 0;
+			}
+			for (int i = string.length() - 1; i >= string.length()-1 - count; i--) {
+				if (string.charAt(i) == '1') {
+					count--;
+					sb.append(0);
+					if (count == 0) {
+						sb.insert(0, string.substring(0, i));
+						return Integer.parseInt(sb.toString(), 2);
+					}
+
+				} else {
+					sb.append(0);
+				}
+
+			}
+			return Integer.parseInt(sb.toString(), 2);
+
+		}
+	}
+
+	public static void main(String[] args) {
+		LeetCode0201 demo = new LeetCode0201();
+		demo.rangeBitwiseAnd(4, 6);
+	}
+
+}
--- src/leetcode/leetcode1001_2000/leetcode1401_1500/leetcode1441_1450/LeetCode1447.java
+++ src/leetcode/leetcode1001_2000/leetcode1401_1500/leetcode1441_1450/LeetCode1447.java
@@ -1,22 +1,52 @@
 package leetcode.leetcode1001_2000.leetcode1401_1500.leetcode1441_1450;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LeetCode1447 {
-	public List<String> simplifiedFractions(int n) {
-		if (n == 1)
-			return new ArrayList<String>();
-		List<String> list = new ArrayList<String>();
-		for (int i = 2; i <= n; i++) {
-			list.add("1/" + i);
-			for (int j = 2; j < i; j++) {
-				if (i % j != 0) {
-					list.add(j + "/" + i);
+	// public List<String> simplifiedFractions(int n) {
+	// if (n == 1)
+	// return new ArrayList<String>();
+	// Map<Double, String> map = new HashMap<Double, String>();
+	// List<String> list = new ArrayList<String>();
+	// for (int i = 2; i <= n; i++) {
+	// list.add("1/" + i);
+	// for (int j = 2; j < i; j++) {
+	// if (i % j != 0) {
+	// if (!map.containsKey(1.0 * i / j)) {
+	// list.add(j + "/" + i);
+	// map.put(1.0 * i / j, "");
+	// }
+	// }
+	// }
+	//
+	// }
+	// return list;
+	// }
+
+	public static List<String> simplifiedFractions(int n) {
+		List<String> res = new ArrayList<>();
+		boolean[][] flag = new boolean[n + 1][n + 1]; // 行是分母，列是分子
+		for (int i = 1; i <= n; i++) { // 初始化为true
+			Arrays.fill(flag[i], true);
+		}
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j < i; j++) {
+				if (flag[i][j]) {
+					int count = 2;
+					while (count * i <= n) { // 分子分母按照自身递增，递增得到的,说明能化简，置为false
+						if (flag[count * i][count * j])
+							flag[count * i][count * j] = false;
+						count++;
+					}
+
+					StringBuffer sb = new StringBuffer();
+					sb.append(j).append("/").append(i);
+					res.add(sb.toString());
 				}
 			}
-
 		}
-		return list;
+		return res;
 	}
 }
