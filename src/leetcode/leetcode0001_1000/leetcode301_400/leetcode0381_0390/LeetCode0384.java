2d1b9f1e80c2677491a374622f484053354878ca
huangjian <1511722165@qq.com>
2021-01-07 11:24:56 +0800

    111

---
+++ src/leetcode/leetcode0001_1000/leetcode301_400/leetcode0381_0390/LeetCode0384.java
@@ -0,0 +1,22 @@
+package leetcode.leetcode0001_1000.leetcode301_400.leetcode0381_0390;
+
+public class LeetCode0384 {
+
+	private int[] nums;
+
+	public LeetCode0384(int[] nums) {
+		this.nums = new int[nums.length];
+		this.nums = nums;
+	}
+
+	/** Resets the array to its original configuration and return it. */
+	public int[] reset() {
+		return this.nums;
+	}
+
+	/** Returns a random shuffling of the array. */
+	public int[] shuffle() {
+
+	}
+
+}
--- src/leetcode/leetcode0001_1000/leetcode701_800/leetcode0741_0750/LeetCode0748.java
+++ src/leetcode/leetcode0001_1000/leetcode701_800/leetcode0741_0750/LeetCode0748.java
@@ -1,5 +1,76 @@
 package leetcode.leetcode0001_1000.leetcode701_800.leetcode0741_0750;
 
+import java.util.Arrays;
+
 public class LeetCode0748 {
 
+	public String shortestCompletingWord(String licensePlate, String[] words) {
+		int[] c = new int[26];
+		int count = 0;
+		int flag = -1;
+		boolean tf = false;
+		int ifg = 0;
+		for (int i = 0; i < licensePlate.length(); i++) {
+			if (licensePlate.charAt(i) >= 'a' && licensePlate.charAt(i) <= 'z') {
+				c[licensePlate.charAt(i) - 'a']++;
+				count++;
+			} else if (licensePlate.charAt(i) >= 'A' && licensePlate.charAt(i) <= 'Z') {
+				c[licensePlate.charAt(i) - 'A']++;
+				count++;
+			}
+		}
+		for (int i = 0; i < words.length; i++) {
+
+			if (flag > 0 && (count + flag) > words[i].length()) {
+
+				int[] temp = Arrays.copyOf(c, 26);
+				for (int j = 0; j < words[i].length(); j++) {
+					temp[words[i].charAt(j) - 'a']--;
+				}
+				tf = true;
+				for (int item : temp) {
+					if (item > 0) {
+						tf = false;
+						break;
+					}
+				}
+				if (tf) {
+
+					if (flag > words[i].length() - count) {
+						ifg = i;
+						flag = words[i].length() - count;
+						if (flag == 0) {
+							return words[i];
+						}
+					}
+
+				}
+
+			} else if (flag < 0 && count <= words[i].length()) {
+				int[] temp = Arrays.copyOf(c, 26);
+				for (int j = 0; j < words[i].length(); j++) {
+					temp[words[i].charAt(j) - 'a']--;
+				}
+				tf = true;
+				for (int item : temp) {
+					if (item > 0) {
+						tf = false;
+						break;
+					}
+				}
+				if (tf) {
+					flag = words[i].length() - count;
+					if (flag == 0) {
+						return words[i];
+					}
+					ifg = i;
+				}
+
+			}
+
+		}
+
+		return words[ifg];
+
+	}
 }
