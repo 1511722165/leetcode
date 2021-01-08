8286381dba6106068650eb58a8e69b4e38d94e6a
huangjian <1511722165@qq.com>
2021-01-08 17:34:47 +0800

    202

--- src/leetcode/leetcode0001_1000/leetcode201_300/leetcode0201_0210/LeetCode0202.java
+++ src/leetcode/leetcode0001_1000/leetcode201_300/leetcode0201_0210/LeetCode0202.java
@@ -2,33 +2,40 @@ package leetcode.leetcode0001_1000.leetcode201_300.leetcode0201_0210;
 
 public class LeetCode0202 {
 
-//	public boolean isHappy(int n) {
-//		int flag = (int) Math.sqrt(n);
-//		int tep = 0;
-//		for (int i = 0; i <= flag; i++) {
-//			for (int j = i; j <= flag; j++) {
-//				tep = (int) Math.sqrt(n - 2 * i * j);
-//				if (n - 2 * i * j == tep * tep) {
-//					return true;
-//				}
-//			}
-//		}
-//		return false;
-//	}
-	
-	
+	// public boolean isHappy(int n) {
+	// int flag = (int) Math.sqrt(n);
+	// int tep = 0;
+	// for (int i = 0; i <= flag; i++) {
+	// for (int j = i; j <= flag; j++) {
+	// tep = (int) Math.sqrt(n - 2 * i * j);
+	// if (n - 2 * i * j == tep * tep) {
+	// return true;
+	// }
+	// }
+	// }
+	// return false;
+	// }
+
 	public boolean isHappy(int n) {
-		boolean flag=false;
-		int tmp=n;
-		while(tmp>9){
-			
-			n=tmp%10;
-			tmp=tmp/10;
-			
-			
+		int tmp = n;
+		while (n > 9) {
+			tmp = n;
+			n = 0;
+			while (tmp > 0) {
+				n += (int) Math.pow(tmp % 10, 2);
+				tmp /= 10;
+
+			}
+
 		}
-		
-		
-		return flag;
+		if (n == 2 || n == 3 || n == 4 || n == 5 || n == 6 || n == 8 || n == 9) {
+			return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		LeetCode0202 demo = new LeetCode0202();
+		demo.isHappy(19);
 	}
 }
