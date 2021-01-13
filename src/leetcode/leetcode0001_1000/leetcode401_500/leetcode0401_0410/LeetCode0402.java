e588c21978941cfc6e831ecbd9a792a58b9c1301
huangjian <1511722165@qq.com>
2021-01-13 12:20:53 +0800

    1

---
+++ src/leetcode/leetcode0001_1000/leetcode401_500/leetcode0401_0410/LeetCode0402.java
@@ -0,0 +1,15 @@
+package leetcode.leetcode0001_1000.leetcode401_500.leetcode0401_0410;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode0402 {
+	public List<String> readBinaryWatch(int num) {
+		if (num > 8)
+			return new ArrayList<String>();
+		List<String> list = new ArrayList<String>();
+		int[] H = { 1, 2, 4, 8 };
+		int[] M = { 1, 2, 4, 8, 16, 32 };
+		return list;
+	}
+}
--- src/leetcode/leetcode1001_2000/leetcode1501_1600/leetcode1501_1510/LeetCode1502.java
+++ src/leetcode/leetcode1001_2000/leetcode1501_1600/leetcode1501_1510/LeetCode1502.java
@@ -3,8 +3,37 @@ package leetcode.leetcode1001_2000.leetcode1501_1600.leetcode1501_1510;
 import java.util.Arrays;
 
 public class LeetCode1502 {
+	// public boolean canMakeArithmeticProgression(int[] arr) {
+	// Arrays.sort(arr);
+	// int cha = arr[1] - arr[0];
+	// if (arr[arr.length - 1] - arr[0] != cha * (arr.length - 1)) {
+	// return false;
+	// } else {
+	// for (int i = 1; i < arr.length; i++) {
+	// if (arr[i] - arr[i - 1] != cha) {
+	// return false;
+	// }
+	// }
+	// }
+	// return true;
+	// }
+	//
+
 	public boolean canMakeArithmeticProgression(int[] arr) {
 		Arrays.sort(arr);
-		
-    }
+		int cha = arr[1] - arr[0];
+		if (arr[arr.length - 1] - arr[0] != cha * (arr.length - 1)) {
+			return false;
+		} else {
+			int i = 1;
+			while (i < arr.length) {
+				if (arr[i] - arr[i - 1] != cha) {
+					return false;
+				}
+				i++;
+			}
+
+		}
+		return true;
+	}
 }
