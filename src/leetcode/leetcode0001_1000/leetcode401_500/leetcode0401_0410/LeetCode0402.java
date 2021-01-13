ed16941b0cdfd0fd065ba1ca5b07ee928286d4a8
huangjian <1511722165@qq.com>
2021-01-14 00:16:48 +0800

    poiu

---
+++ src/leetcode/leetcode0001_1000/leetcode401_500/leetcode0401_0410/LeetCode0402.java
@@ -0,0 +1,10 @@
+package leetcode.leetcode0001_1000.leetcode401_500.leetcode0401_0410;
+
+public class LeetCode0402 {
+	 public String removeKdigits(String num, int k) {
+		 if(num.length()==k){
+			 return "0";
+		 }
+		 return "";
+	    }
+}
