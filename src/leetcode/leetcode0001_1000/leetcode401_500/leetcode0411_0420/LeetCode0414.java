6cf401ea48e51f37bb81f8337476bd78a0fc12b6
huangjian <1511722165@qq.com>
2021-01-14 23:37:08 +0800

    1

--- src/leetcode/leetcode0001_1000/leetcode401_500/leetcode0411_0420/LeetCode0414.java
+++ src/leetcode/leetcode0001_1000/leetcode401_500/leetcode0411_0420/LeetCode0414.java
@@ -5,8 +5,33 @@ public class LeetCode0414 {
 		int max = nums[0];
 		int maxs = nums[0];
 		int maxt = nums[0];
-		for (int i = 1; i < nums.length; i++) {
+		int i = 1;
+		while (i < nums.length) {
+			if (nums[i] != max && nums[i] != maxs && nums[i] != maxt) {
+				if (nums[i] > max) {
+					maxt = maxs;
+					maxs = max;
+					max = nums[i];
+				} else if (nums[i] > maxs && nums[i] < max) {
+					maxt = maxs;
+					maxs = nums[i];
+				} else if (nums[i] > maxt) {
+					maxt = nums[i];
+				} else {
 
+					if (maxs == maxt && max == maxs) {
+						maxs = nums[i];
+						maxt = nums[i];
+					} else if (maxs == maxt) {
+						maxt = nums[i];
+					}
+
+				}
+			}
+			i++;
+		}
+		if (maxs == maxt) {
+			return max;
 		}
 		return maxt;
 	}
