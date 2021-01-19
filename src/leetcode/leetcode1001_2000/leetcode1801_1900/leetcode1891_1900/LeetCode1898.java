835c5855c884128a03b3ee7d4bd75431c0c7e689
huangjian <1511722165@qq.com>
2021-01-20 00:23:22 +0800

    2

--- src/leetcode/leetcode1001_2000/leetcode1801_1900/leetcode1891_1900/LeetCode1898.java
+++ src/leetcode/leetcode1001_2000/leetcode1801_1900/leetcode1891_1900/LeetCode1898.java
@@ -5,16 +5,37 @@ import java.util.Arrays;
 public class LeetCode1898 {
 	public int smallestDifference(int[] a, int[] b) {
 		Arrays.sort(a);
-		long min1 = Math.abs((long) (a[0] - b[0]));
-		long min2 = Math.abs((long) (a[1] - b[0]));
-		for (int j = 0; j < b.length; j++) {
-			min1 = Math.min(min1, Math.abs((long) (a[0] - b[j])));
-			min2 = Math.min(min1, Math.abs((long) (a[1] - b[j])));
-		}
-		if(min2<min1){}
+		Arrays.sort(b);
+//		if (a[a.length - 1] <= b[0]) {
+//			return Math.abs(b[0] - a[a.length - 1]);
+//		} else if (b[b.length - 1] <= a[0]) {
+//			return Math.abs(a[0] - b[b.length - 1]);
+//		}
+		long min1  = Long.MAX_VALUE;;
+		int flag = 0;
 		for (int i = 0; i < a.length; i++) {
-			
+			int j = flag;
+			while (j < b.length) {
+				if (a[i] == b[j]) {
+					return 0;
+				} else if (a[i] > b[j]) {
+					min1 = Math.min(min1, Math.abs((long) (a[i] - b[j])));
+					flag++;
+					j++;
+				} else if (a[i] < b[j]) {
+					min1 = Math.min(min1, Math.abs((long) (a[i] - b[j])));
+					flag = Math.max(0, j - 1);
+					j = b.length;
+				}
+			}
 		}
 		return (int) min1;
+	}
+
+	public static void main(String[] args) {
+		LeetCode1898 demo = new LeetCode1898();
+		int[] a = { -2147483648, 1 };
+		int[] b = { 2147483647, 0 };
+		demo.smallestDifference(a, b);
 	}
 }
