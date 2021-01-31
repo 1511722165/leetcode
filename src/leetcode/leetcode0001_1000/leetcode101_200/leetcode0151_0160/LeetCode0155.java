c5503b4686b8e903a000cc8dc35d0844fe97f90d
huangjian <1511722165@qq.com>
2021-02-01 00:13:15 +0800

    x

---
+++ src/leetcode/leetcode0001_1000/leetcode101_200/leetcode0151_0160/LeetCode0155.java
@@ -0,0 +1,25 @@
+package leetcode.leetcode0001_1000.leetcode101_200.leetcode0151_0160;
+
+public class LeetCode0155 {
+
+	int min;
+	public LeetCode0155() {
+
+	}
+
+	public void push(int x) {
+
+	}
+
+	public void pop() {
+
+	}
+
+	public int top() {
+		return 0;
+	}
+
+	public int getMin() {
+		return 0;
+	}
+}
