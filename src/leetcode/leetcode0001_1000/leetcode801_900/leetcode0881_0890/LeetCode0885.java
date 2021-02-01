68bcc343e8b021e40e01bc6afac6b55b9e6ff8ec
huangjian <1511722165@qq.com>
2021-02-01 21:28:26 +0800

    1

---
+++ src/leetcode/leetcode0001_1000/leetcode801_900/leetcode0881_0890/LeetCode0885.java
@@ -0,0 +1,14 @@
+package leetcode.leetcode0001_1000.leetcode801_900.leetcode0881_0890;
+
+public class LeetCode0885 {
+	public int[][] spiralMatrixIII(int R, int C, int r0, int c0) {
+		int[][] res = new int[R][C];
+		int index = Math.multiplyExact(r0, c0);
+		int flag = 0;
+		while (index > 0) {
+
+		}
+
+		return res;
+	}
+}
