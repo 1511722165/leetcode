e5520be7479ddc3378cbe5c11627171984218cd0
huangjian <1511722165@qq.com>
2021-02-02 11:24:19 +0800

    1

---
+++ src/leetcode/leetcode1001_2000/leetcode1801_1900/leetcode1841_1850/LeetCode1842.java
@@ -0,0 +1,13 @@
+package leetcode.leetcode1001_2000.leetcode1801_1900.leetcode1841_1850;
+
+public class LeetCode1842 {
+
+	public int sumNums(int n) {
+		int sum = 0, index = 1;
+		while (index <= n) {
+			sum += index;
+			index++;
+		}
+		return sum;
+	}
+}
