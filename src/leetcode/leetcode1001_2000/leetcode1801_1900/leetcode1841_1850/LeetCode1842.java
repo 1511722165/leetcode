2ab70c0774dc1915d6d6c1c0b0786c17a760233a
huangjian <1511722165@qq.com>
2021-02-03 11:15:24 +0800

    5

--- src/leetcode/leetcode1001_2000/leetcode1801_1900/leetcode1841_1850/LeetCode1842.java
+++ src/leetcode/leetcode1001_2000/leetcode1801_1900/leetcode1841_1850/LeetCode1842.java
@@ -3,11 +3,12 @@ package leetcode.leetcode1001_2000.leetcode1801_1900.leetcode1841_1850;
 public class LeetCode1842 {
 
 	public int sumNums(int n) {
-		int sum = 0, index = 1;
-		while (index <= n) {
-			sum += index;
-			index++;
-		}
-		return sum;
+//		int sum = 0, index = 1;
+//		while (index <= n) {
+//			sum += index;
+//			index++;
+//		}
+//		return sum;
+		return (int) (Math.pow(n, 2)+ n) >>1;
 	}
 }
