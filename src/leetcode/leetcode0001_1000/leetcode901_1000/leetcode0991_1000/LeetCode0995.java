7e0995b34db7aabd36b486df31e8151677d711e1
huangjian <1511722165@qq.com>
2021-02-18 17:34:53 +0800

    1

---
+++ src/leetcode/leetcode0001_1000/leetcode901_1000/leetcode0991_1000/LeetCode0995.java
@@ -0,0 +1,11 @@
+package leetcode.leetcode0001_1000.leetcode901_1000.leetcode0991_1000;
+
+public class LeetCode0995 {
+	  public int minKBitFlips(int[] A, int K) {
+
+		  int res=-1;
+		  
+		  
+		  return res;
+	    }
+}
