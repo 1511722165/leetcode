2b12c55e0f71b1fedf9632c0b8bf7bd25efcbb35
huangjian <1511722165@qq.com>
2021-02-23 09:40:50 +0800

    w

--- src/leetcode/leetcode1001_2000/leetcode1801_1900/leetcode1861_1870/LeetCode1870.java
+++ src/leetcode/leetcode1001_2000/leetcode1801_1900/leetcode1861_1870/LeetCode1870.java
@@ -3,19 +3,51 @@ package leetcode.leetcode1001_2000.leetcode1801_1900.leetcode1861_1870;
 import java.util.Arrays;
 
 public class LeetCode1870 {
-	public int[] constructArr(int[] a) {
+	public int[] constructArr1(int[] a) {
 		int[] b = new int[a.length];
 		int ji = 1;
+		int count = 0;
 		for (int item : a) {
-			ji *= item;
+			if (item != 0) {
+				ji *= item;
+			} else {
+				count++;
+			}
 		}
-		for (int i = 0; i < a.length; i++) {
-			if(a[i]==0){
-				b[i]=ji;
-			}else{
-				b[i] = ji / a[i];
+		if (count == 1) {
+			for (int i = 0; i < a.length; i++) {
+				if (a[i] == 0) {
+					b[i] = ji;
+				}
+			}
+		} else if (count > 1) {
+			return b;
+		} else {
+
+			for (int i = 0; i < a.length; i++) {
+				if (a[i] == 0) {
+					b[i] = ji;
+				} else {
+					b[i] = ji / a[i];
+				}
 			}
 		}
 		return b;
+	}
+
+	public int[] constructArr(int[] a) {
+		int n = a.length;
+		int[] B = new int[n];
+		for (int i = 0, product = 1; i < n; product *= a[i], i++) /* ¥”◊ÛÕ˘”“¿€≥À */
+			B[i] = product;
+		for (int i = n - 1, product = 1; i >= 0; product *= a[i], i--) /* ¥””“Õ˘◊Û¿€≥À */
+			B[i] *= product;
+		return B;
+	}
+
+	public static void main(String[] args) {
+		LeetCode1870 demo = new LeetCode1870();
+		int[] a = { 1, 2, 3, 4, 5 };
+		demo.constructArr(a);
 	}
 }
