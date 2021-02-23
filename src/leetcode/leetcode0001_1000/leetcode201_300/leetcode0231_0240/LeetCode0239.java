a87646cce271dbafe4b88157f8ed0c88b03a6092
huangjian <1511722165@qq.com>
2021-02-23 20:11:54 +0800

    1

---
+++ src/leetcode/leetcode0001_1000/leetcode201_300/leetcode0231_0240/LeetCode0239.java
@@ -0,0 +1,74 @@
+package leetcode.leetcode0001_1000.leetcode201_300.leetcode0231_0240;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LeetCode0239 {
+	public int[] maxSlidingWindow1(int[] nums, int k) {
+		if (nums == null || nums.length == 0) {
+			return new int[0];
+		}
+		int[] res = new int[nums.length - k + 1];
+		Deque<Integer> queue = new ArrayDeque<>();
+		for (int i = 0, j = 0; i < nums.length; i++) {
+			// first 最先的
+			if (!queue.isEmpty() && i - queue.peek() >= k) {
+				queue.poll();
+			}
+			// peekLast 相当于getlast
+			while (!queue.isEmpty() && nums[i] > nums[queue.peekLast()]) {
+				// remove last
+				queue.pollLast();
+			}
+			queue.offer(i);
+			if (i >= k - 1) {
+				res[j++] = nums[queue.peek()];
+			}
+		}
+
+		return res;
+	}
+
+	public int[] maxSlidingWindow(int[] nums, int k) {
+		int[] res = new int[nums.length - k + 1];
+		int left = 0;
+		int right = left + k - 1;
+		if (right >= nums.length)
+			return res;
+		int maxIndex = maxNum(nums, 0, right);
+		while (true) {
+			// 如果最大值还在窗口内
+			if (left <= maxIndex) {
+				// 记录结果并移动左指针
+				res[left++] = nums[maxIndex];
+				// 移动右指针
+				// 如果新的元素大于当前最大值，则进行更新
+				++right;
+				if (right == nums.length)
+					break;
+				if (nums[right] >= nums[maxIndex])
+					maxIndex = right;
+			} else {
+				// 最大值从左侧移出窗口内了
+				// 判断边界值，一定程度避免遍历寻找
+				if (nums[left] >= nums[maxIndex] - 1)
+					maxIndex = left;
+				else if (nums[right] >= nums[maxIndex] - 1)
+					maxIndex = right;
+				else
+					maxIndex = maxNum(nums, left, right);
+			}
+		}
+		return res;
+	}
+
+	public int maxNum(int[] nums, int start, int end) {
+		int index = start;
+		for (int i = start + 1; i <= end; i++) {
+			if (nums[i] >= nums[index]) {
+				index = i;
+			}
+		}
+		return index;
+	}
+}
--- src/leetcode/leetcode1001_2000/leetcode1801_1900/leetcode1861_1870/LeetCode1862.java
+++ src/leetcode/leetcode1001_2000/leetcode1801_1900/leetcode1861_1870/LeetCode1862.java
@@ -1,22 +1,53 @@
 package leetcode.leetcode1001_2000.leetcode1801_1900.leetcode1861_1870;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class LeetCode1862 {
-	public int[] maxSlidingWindow(int[] nums, int k) {
+	public int[] maxSlidingWindow1(int[] nums, int k) {
+
 		int n = nums.length;
 		int[] res = new int[n - k + 1];
-		int max1 = 0;
-		int max2=0;
-		for (int i = 0; i < k; i++) {
-			if(nums[i]>max1){
-				max2=max1;
-				max1=nums[i];
-			}else{}
-		}
-		res[0] = count;
-		for (int i = 1; i + k - 1 < n; i++) {
-			count = count + nums[i + k - 1] - nums[i - 1];
-			res[i] = count;
+		int max;
+		for (int i = 0; i + k - 1 < n; i++) {
+			max = nums[i];
+			for (int j = 1; j < k; j++) {
+				max = Math.max(max, nums[i + j]);
+			}
+			res[i] = max;
 		}
 		return res;
+	}
+
+	public int[] maxSlidingWindow(int[] nums, int k) {
+		if (nums == null || nums.length == 0) {
+			return new int[0];
+		}
+		int[] res = new int[nums.length - k + 1];
+		Deque<Integer> queue = new ArrayDeque<>();
+		for (int i = 0, j = 0; i < nums.length; i++) {
+			// first 最先的
+			if (!queue.isEmpty() && i - queue.peek() >= k) {
+				queue.poll();
+			}
+			// peekLast 相当于getlast
+			while (!queue.isEmpty() && nums[i] > nums[queue.peekLast()]) {
+				// remove last
+				queue.pollLast();
+			}
+			queue.offer(i);
+			if (i >= k - 1) {
+				res[j++] = nums[queue.peek()];
+			}
+		}
+
+		return res;
+	}
+
+	public static void main(String[] args) {
+		LeetCode1862 demo = new LeetCode1862();
+		int nums[] = { 1, 3, -1, -3, 5, 3, 6, 7 };
+		int k = 3;
+		demo.maxSlidingWindow(nums, k);
 	}
 }
