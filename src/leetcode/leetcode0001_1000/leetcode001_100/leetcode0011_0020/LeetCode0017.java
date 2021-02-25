d123d05ba36dd2b0f0562a51d689b1753fba2edd
huangjian <1511722165@qq.com>
2021-02-25 20:14:26 +0800

    FD

---
+++ src/leetcode/leetcode0001_1000/leetcode001_100/leetcode0011_0020/LeetCode0017.java
@@ -0,0 +1,16 @@
+package leetcode.leetcode0001_1000.leetcode001_100.leetcode0011_0020;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode0017 {
+	public List<String> letterCombinations(String digits) {
+		if (digits == null || digits.length() == 0) {
+			return new ArrayList<String>();
+		}
+		
+		
+		return new ArrayList<String>();
+	}
+
+}
