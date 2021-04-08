a29b7465e9a38c46a78c011f7b24d39f17d0cf9c
huangjian <1511722165@qq.com>
2021-04-08 11:35:58 +0800

    ?

---
+++ src/leetcode/leetcode0001_1000/leetcode101_200/leetcode0151_0160/LeetCode0153.java
@@ -0,0 +1,10 @@
+package leetcode.leetcode0001_1000.leetcode101_200.leetcode0151_0160;
+
+import java.util.Arrays;
+
+public class LeetCode0153{
+        public int findMin(int[] nums) {
+            Arrays.sort(nums);
+            return nums[0];
+        }
+}
