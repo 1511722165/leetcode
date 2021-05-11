bb76c838df2086163ca41c16debd03609429befa
huangjian <1511722165@qq.com>
2021-05-11 17:02:59 +0800

    2

---
+++ src/leetcode/leetcode1001_2000/leetcode1701_1800/leetcode1731_1740/LeetCode1734.java
@@ -0,0 +1,14 @@
+package leetcode.leetcode1001_2000.leetcode1701_1800.leetcode1731_1740;
+
+public class LeetCode1734 {
+
+    public int[] decode(int[] encoded) {
+    int[] res =new int[encoded.length+1];
+
+    int i=1;
+
+
+
+    return res;
+    }
+}
