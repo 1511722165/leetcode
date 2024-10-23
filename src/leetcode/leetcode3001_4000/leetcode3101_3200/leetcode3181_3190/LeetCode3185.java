29890cc91db82979cd97a03fc1c0095afeb15352
1511722165@qq.com <huang1995jian>
2024-10-23 08:46:48 +0800

    1

--- src/leetcode/leetcode3001_4000/leetcode3101_3200/leetcode3181_3190/LeetCode3185.java
+++ src/leetcode/leetcode3001_4000/leetcode3101_3200/leetcode3181_3190/LeetCode3185.java
@@ -1,13 +1,13 @@
 package leetcode.leetcode3001_4000.leetcode3101_3200.leetcode3181_3190;
 
-public class LeetCode3184 {
+public class LeetCode3185 {
 
-    public int countCompleteDayPairs(int[] hours) {
-        int[] arr = new int[24];
+    public long countCompleteDayPairs(int[] hours) {
+        long[] arr = new long[24];
         for (int i : hours) {
             arr[i % 24]++;
         }
-        int res = 0;
+        long res = 0;
         res += (arr[0] * (arr[0] - 1)) / 2;
         res += (arr[12] * (arr[12] - 1)) / 2;
         for (int i = 1; i <= 11; i++) {
@@ -15,5 +15,4 @@ public class LeetCode3184 {
         }
         return res;
     }
-
 }
