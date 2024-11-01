160d76cd20e5e5f44f032e1242fb534987875382
黄健 <wb.huangjian08@mesg.corp.netease.com>
2024-11-01 15:40:23 +0800

    1

--- src/leetcode/leetcode0001_1000/leetcode001_100/leetcode0001_0010/LeetCode0001.java
+++ src/leetcode/leetcode0001_1000/leetcode001_100/leetcode0001_0010/LeetCode0001.java
@@ -42,15 +42,15 @@ public class LeetCode0001 {
 	}
 
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		// 注意 hasNext 和 hasNextLine 的区别
-		while (in.hasNext()) { // 注意 while 处理多个 case
-			String a = in.nextLine();
-			if(a.lastIndexntOf(' ')<0)System.out.println(a.length());
-			System.out.println(a.length() - a.lastIndexOf(' '));
-		}
-	}
+//	public static void main(String[] args) {
+//		Scanner in = new Scanner(System.in);
+//		// 注意 hasNext 和 hasNextLine 的区别
+//		while (in.hasNext()) { // 注意 while 处理多个 case
+//			String a = in.nextLine();
+//			if(a.lastIndexntOf(' ')<0)System.out.println(a.length());
+//			System.out.println(a.length() - a.lastIndexOf(' '));
+//		}
+//	}
 
 
 
